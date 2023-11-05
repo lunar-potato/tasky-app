2f762fcf8eac218122968c09be3128a20f0dc267
webtam81 <t.h@tammyharris.co.uk>
2023-11-05 02:30:59 +0000

    form styles

--- src/app/components/TaskContainer.jsx
+++ src/app/components/TaskContainer.jsx
@@ -20,7 +20,7 @@ const TaskContainer = ({ title }) => {
         <TaskCard tasks={taskData} />
       </div>
       <div>
-        <button className="flex items-start font-bold hover:text-white">+ Add Card</button>
+        <button className="flex items-start font-bold text-white hover:text-teal-100">+ Add Card</button>
       </div>
     </div>
   );
