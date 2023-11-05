e3e69eb6525a7ca51605088b4e4f3c5bc55dc00d
webtam81 <t.h@tammyharris.co.uk>
2023-11-05 01:41:43 +0000

    main nav bar with logo added. colour schemes.

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav>
-      <h1>Navbar</h1>
+    <nav className="justify-center p-8 text-center bg-slate-500">
+      <img className="inline" src="./tasky-200-w-t.png" width="150" alt="Tasky logo" />
     </nav>
   );
 };
