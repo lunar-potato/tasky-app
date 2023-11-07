a3d05e8efdd99af3b993046c35cda68d11d9dc4e
Memouna Irain <memounairain@Memounas-MacBook-Pro.local>
2023-11-07 22:27:50 +0000

    added navbar and made changes to taskcard so it is clickable and expandable

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <nav className="justify-center p-8 text-center bg-slate-500">
-      <img className="inline" src="./tasky-200-w-t.png" width="150" alt="Tasky logo" />
+    <nav className="nav justify-center p-8 text-center bg-slate-500">
+    <img className="inline" src="./tasky-200-w-t.png" width="150" alt="Tasky logo" />
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
     </nav>
   );
-};
-
+}
 
 export default Navbar;
