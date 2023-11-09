9cece49bcffd690d3a62d3b5eae162ad2e2731c8
luna potato <gwynethbautistaa@gmail.com>
2023-11-09 18:19:30 +0000

    FIXED DND

--- src/app/components/TaskContainer.jsx
+++ src/app/components/TaskContainer.jsx
@@ -1,86 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
 import { MoreVertical } from "lucide-react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import TaskCard from "./TaskCard";
 import TaskModal from "./TaskModal";
 import AddTask from "./AddTask";
 
-const supabaseUrl = "https://vumfiwtseuqdsodbaghp.supabase.co";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
 
-const TaskContainer = ({ type, droppableId }) => {
-  const [tasks, setTasks] = useState([]);
+const TaskContainer = ({ type, tasks }) => {
   const [showAddTaskModal, setShowAddTaskModal] = useState(false);
-
-  useEffect(() => {
-    async function fetchTasks() {
-      const { data, error } = await supabase
-        .from("tasks")
-        .select()
-        .eq("taskType", type);
-
-      if (error) {
-        console.error("Error fetching tasks: ", error);
-      } else {
-        setTasks(data);
-      }
-    }
-
-    fetchTasks();
-  }, [type]);
-
-  const addTaskCallback = (newTask) => {
-    console.log("Adding task:", newTask);
-    setTasks([...tasks, newTask]);
-    setShowAddTaskModal(false);
-  };
-
-  const handleDragEnd = async (result) => {
-    if (!result.destination) {
-      return;
-    }
-
-    const sourceColumn = result.source.droppableId;
-    const destinationColumn = result.destination.droppableId;
-
-    const updatedTasks = [...tasks];
-    const [reorderedTask] = updatedTasks.splice(result.source.index, 1);
-    updatedTasks.splice(result.destination.index, 0, reorderedTask);
-
-    updatedTasks.forEach((task, index) => {
-      task.order = index;
-    });
-
-    setTasks(updatedTasks);
-
-    const newOrder = updatedTasks.map((task) => ({
-      id: task.id,
-      order: task.order,
-    }));
-
-    const { error } = await supabase
-      .from("tasks")
-      .upsert(newOrder, { onConflict: ["id"], returning: "minimal" });
-
-    if (error) {
-      console.error("Error updating task order: ", error);
-    }
-
-    if (sourceColumn !== destinationColumn) {
-      const taskToMove = tasks[result.source.index];
-
-      const { error } = await supabase
-        .from("tasks")
-        .update({ taskType: destinationColumn })
-        .eq("id", taskToMove.id);
-
-      if (error) {
-        console.error("Error updating task type: ", error);
-      }
-    }
-  };
 
   return (
     <div className="p-2 border-white border-x-2 md:border-x-0 xl:m-4 md:shadow-lg xl:p-4 md:rounded bg-slate-500">
@@ -91,8 +18,7 @@ const TaskContainer = ({ type, droppableId }) => {
         </button>
       </div>
 
-      <DragDropContext onDragEnd={handleDragEnd}>
-        <Droppable droppableId={droppableId}>
+        <Droppable droppableId={type}>
           {(provided) => (
             <ul ref={provided.innerRef} {...provided.droppableProps}>
               <TaskCard tasks={tasks} />
@@ -100,7 +26,6 @@ const TaskContainer = ({ type, droppableId }) => {
             </ul>
           )}
         </Droppable>
-      </DragDropContext>
 
       <div>
         <button
@@ -114,7 +39,7 @@ const TaskContainer = ({ type, droppableId }) => {
             isOpen={showAddTaskModal}
             onClose={() => setShowAddTaskModal(false)}
           >
-            <AddTask onClose={addTaskCallback} />
+            <AddTask />
           </TaskModal>
         )}
       </div>
