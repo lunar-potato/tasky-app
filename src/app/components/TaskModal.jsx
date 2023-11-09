557f752ce89351275575b6760dfa9b223bd171b5
webtam81 <t.h@tammyharris.co.uk>
2023-11-09 10:13:46 +0000

    colour coding, task buttons and labels

--- src/app/components/TaskModal.jsx
+++ src/app/components/TaskModal.jsx
@@ -7,7 +7,7 @@ const TaskModal = ({ isOpen, onClose, children }) => {
     <AnimatePresence>
       {isOpen && (
         <motion.div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+          className="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
